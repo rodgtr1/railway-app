2a64da9ef8228a24b93bfa0cf4427ec9e1b289f3
Travis Rodgers <travisdotmedia@gmail.com>
2024-09-03 13:07:11 -0400

    cleanup

--- app/Http/Controllers/ThumbnailController.php
+++ app/Http/Controllers/ThumbnailController.php
@@ -22,17 +22,18 @@ class ThumbnailController extends Controller
             'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif,svg,webp|max:10240',
         ]);
 
-        // If there's a profile photo in the request, store it.
         if ($request->hasFile('thumbnail')) {
             $file = $request->file('thumbnail');
             $filePath = $request->user()->id.'/'.$file->getClientOriginalName();
             $path = $file->storeAs($filePath);
             $request->user()->thumbnail = $filePath;
-        }
 
-        $request->user()->thumbnails()->create([
-            'name' => $file->getClientOriginalName(),
-        ]);
+            $request->user()->thumbnails()->create([
+                'name' => $file->getClientOriginalName(),
+            ]);
+        } else {
+            return redirect()->back()->with('error', 'Failed to upload thumbnail.');
+        }
 
         return redirect(route('dashboard'))->with('status', 'thumbnail-uploaded');
     }
@@ -47,20 +48,16 @@ class ThumbnailController extends Controller
 
         RemoveBackground::dispatch($request->user(), $thumbnailUrl);
 
-        // Redirect back with a success message
         return redirect()->back()->with('status', 'background-job-initiated');
 
     }
 
     public function destroy(Request $request,Thumbnail $thumbnail)
     {
-        // Delete database thumbnail
         $thumbnail->delete();
 
-        // Delete thumbnail from MinIO
         Storage::delete($request->user()->id . '/' . $thumbnail->name);
 
-        // Redirect back with a success message
         return redirect()->back()->with('success', 'Thumbnail deleted successfully.');
     }
 
